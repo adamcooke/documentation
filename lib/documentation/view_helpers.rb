7a3f4432a5c953e83ea6a357411fa3aba281801d
Adam Cooke <adam@atechmedia.com>
2014-11-02 16:02:06 +0000

    check permissions when rendering root nav

--- lib/documentation/view_helpers.rb
+++ lib/documentation/view_helpers.rb
@@ -1,44 +1,44 @@
 module Documentation
   module ViewHelpers
-    
+
     #
     # Path to edit a page in the manager UI
     #
     def documentation_edit_page_path(page)
       "#{::Documentation::Engine.mounted_path}/edit/#{page.full_permalink}"
     end
-    
+
     #
     # Path to view a page in the manager UI
     #
     def documentation_page_path(page)
       "#{::Documentation::Engine.mounted_path}/#{page.try(:full_permalink)}"
     end
-    
+
     #
     # Return a breadcrumb for the given page
     #
     def documentation_breadcrumb_for(page, options = {})
       options[:root_link] = options[:root_link].nil? ? t('documentation.helpers.documentation_breadcrumb_for.default_root_link') : options[:root_link]
       options[:class]     ||= 'breadcrumb'
-      
+
       String.new.tap do |s|
         s << "<nav class='#{options[:class]}'><ul>"
-        
+
         if options[:root_link]
           s << "<li><a href='#{documentation_doc_root.blank? ? '/' : documentation_doc_root}'>#{options[:root_link]}</a></li>"
         end
-        
+
         if page.is_a?(::Documentation::Page)
           page.breadcrumb.each do |child|
             s << "<li><a href='#{documentation_doc_root}/#{child.full_permalink}'>#{child.title}</a></li>"
           end
         end
-        
+
         s << "</ul></nav>"
       end.html_safe
     end
-    
+
     #
     # Return a default navigation tree for the given page
     #
@@ -56,39 +56,39 @@ module Documentation
               end
               s << "</ul>"
             end
-            s << "</li>"            
+            s << "</li>"
           end
         else
-          ::Documentation::Page.roots.each do |page|
+          ::Documentation::Page.roots.select { |p| documentation_authorizer.can_view_page?(p) }.each do |page|
             s << "<li><a href='#{documentation_doc_root}/#{page.full_permalink}'>#{page.title}</a></li>"
           end
         end
         s << "</ul>"
       end.html_safe
     end
-    
+
     #
     # Return appropriate content for a given page
     #
     def documentation_content_for(page)
       # Get the content
       content = page.compiled_content.to_s
-      
+
       # Insert navigation
-      content.gsub!("<p class=''>{{nav}}</p>") do 
+      content.gsub!("<p class=''>{{nav}}</p>") do
         children = page.children
         children = children.select { |c| documentation_authorizer.can_view_page?(c) }
         items = children.map { |c| "<li><a href='#{documentation_doc_root}/#{c.full_permalink}'>#{c.title}</a></li>" }.join
         "<ul class='pages'>#{items}</ul>"
       end
-      
+
       # Set the document root as appropriate
       content.gsub!("{{docRoot}}", documentation_doc_root)
-      
+
       # Return HTML safe content
       content.html_safe
     end
-    
+
     #
     # Return the documentation document root
     #
@@ -101,21 +101,21 @@ module Documentation
         end
       end
     end
-    
+
     #
-    # Return the documentation authorizer 
+    # Return the documentation authorizer
     #
     def documentation_authorizer
       @documentation_authorizer ||= Documentation.config.authorizer.new(controller)
     end
-    
-    # 
+
+    #
     # Return summary information for search results
     #
     def documentation_search_summary(result)
       t('documentation.helpers.documentation_search_summary.text', :total_results => result.total_results, :start_result => result.start_result_number, :end_result => result.end_result_number, :query => result.query)
     end
-    
+
     #
     # Return the search results
     #
@@ -137,7 +137,7 @@ module Documentation
         s << "</ul>"
       end.html_safe
     end
-    
+
     #
     # Return search pagination links
     #
@@ -154,6 +154,6 @@ module Documentation
         end
       end.html_safe
     end
-    
+
   end
 end
